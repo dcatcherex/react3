7f107f068c44846d48b459f1f9a3b2739a6557d0
dcatcherex <dcatcherex@yahoo.com>
2022-04-17 23:07:49 +0700

    update

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -1,61 +1,99 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import styles from "./Form.module.css";
 
-const Form = () => { 
-    const [inputs,setInputs] = useState('');
+const Form = (props) => {
+  const { activity, date, topic, des, hour, minute, image, tag } = props;
+  const [inputs, setInputs] = useState("");
 
-    const handleChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setInputs(values => ({...values, [name]:value}))
-    }
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert(inputs)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      data: {
+        activity: activity,
+        topic: topic,
+        description: des,
+        image: image,
+        hour: hour,
+        minute: minute,
+        tag: tag,
+        date: date,
+      },
+    });
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>Activity
-                <input 
-                    type="text" 
-                    name="activity"
-                    value={inputs.activity || ""} 
-                    onChange={handleChange}
-                />
-            </label>
-            <label>Description
-                <input 
-                    type="text" 
-                    name="description"
-                    value={inputs.description || ""} 
-                    onChange={handleChange}
-                />
-            </label>
-            <label>Date
-                <input 
-                    type="text" 
-                    name="date"
-                    value={inputs.date || ""} 
-                    onChange={handleChange}
-                />
-            </label>
-            <label>Duration
-                <input 
-                    type="text" 
-                    name="duration"
-                    value={inputs.duration || ""} 
-                    onChange={handleChange}
-                />
-            </label>
-            
-            <input type="submit" />
+  return (
+    <form className={styles.Form} onSubmit={handleSubmit}>
+      <label>
+        Activity
+        <input
+          type="text"
+          name="activity"
+          value={inputs.activity || ""}
+          onChange={handleChange}
+          placeholder="ว่ายน้ำ"
+        />
+      </label>
+      <label>
+        Date
+        <input
+          type="date"
+          name="date"
+          value={inputs.date || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Topic
+        <input
+          type="text"
+          name="topic"
+          value={inputs.topic || ""}
+          onChange={handleChange}
+          placeholder="ว่ายน้ำที่คอนโด"
+        />
+      </label>
+      <label>
+        Description
+        <input
+          type="text"
+          name="description"
+          value={inputs.description || ""}
+          onChange={handleChange}
+          placeholder="วันนี้ร้อนมากเลยไปว่ายน้ำเล่นที่คอนโดเพื่อน"
+        />
+      </label>
+      Duration
+      <label>
+        Hour
+        <input
+          type="number"
+          name="hour"
+          value={inputs.hour || ""}
+          onChange={handleChange}
+          placeholder="0"
+        />
+      </label>
+      <label>
+        Minute
+        <input
+          type="number"
+          name="minute"
+          value={inputs.hour || ""}
+          onChange={handleChange}
+          placeholder="0"
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  );
+};
 
-        </form>
-    )
-}
-
-export default Form
-
-
- 
+export default Form;
--- src/routes/Blog.js
+++ src/routes/Blog.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BlogPage } from "../components/BlogPage/BlogPage";
+import Form from "../components/Forms/Form";
 
 export const Blog = (props) => {
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Blog</h2>
       <BlogPage />
+      <Form />
     </main>
   );
 };
